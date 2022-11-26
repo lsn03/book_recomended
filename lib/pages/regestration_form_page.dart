5eb01bf705b4b3e0aa0d0809ea4cb6f93e33e828
lsn03 <graffgray8@gmail.com>
2022-11-26 13:39:55 +1000

    made first ui of reg

--- lib/pages/regestration_form_page.dart
+++ lib/pages/regestration_form_page.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
@@ -23,15 +25,89 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
           children: [
             TextField(
               decoration: InputDecoration(
-                labelText: "First name*",
+                labelText: "Имя *",
               ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Фамилия",
+              ),
+            ),
+            SizedBox(
+              height: 10,
             ),
             TextFormField(
-              decoration: InputDecoration(
-                labelText: "Email*"
-              ),
+              decoration: InputDecoration(labelText: "Почта *"),
             ),
-
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Пароль *"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Повторите пароль *"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Социальная сеть"),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Дата рождения *"),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: 30,
+                  width: 125,
+                  child: ElevatedButton(
+                    onPressed: (() {}),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 252, 255, 220),
+                    ),
+                    child: Text(
+                      "Назад",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,  
+                        ),
+                        
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                  width: 125,
+                  child: ElevatedButton(
+                    onPressed: (() {}),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFD0FFD2),
+                      
+                    ),
+                    
+                    child: Text(
+                      "Отправить",
+                      style: TextStyle(fontSize: 15,color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),

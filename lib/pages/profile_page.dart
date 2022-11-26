ad9a2c47e1079dea7904196d18aae1ab3d814ce2
lsn03 <graffgray8@gmail.com>
2022-11-27 00:53:12 +1000

    fix profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -8,9 +8,114 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.black,
+              )),
+        ],
+        automaticallyImplyLeading: false,
+        //toolbarHeight: 0,
+        backgroundColor: Color(0xFFD1FFD2),
+      ),
+      body: Profile(),
+    );
+  }
+}
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //mainAxisAlignment: MainAxisAlignment.center,
+      //crossAxisAlignment: CrossAxisAlignment.center,
+
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              
+              Column(
+                //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                  ),
+                  Text("Ирина"),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+              //mainAxisAlignment: MainAxisAlignment.start,
         
-      ) ,
+              children: [
+                
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Нравится: "),
+                    Text("Не нравится: "),
+                    Text("Хочет прочитать: "),
+                    Text("Соц.сеть: "),
+                  ],
+                ),
+                SizedBox(width: 120,),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    
+                    Text(" 125 книг "),
+                    Text("13 книг"),
+                    Text("200 книг"),
+                    Text("t.me/reenashka"),
+                  ],
+                ),
+                
+              ]),
+        ),
+        Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Предпочитаемые жанры:"),
+                   Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                      
+                      Text("Фентези, приключения,современная проза, пьесы, научная фантастика, комедия, драма"),
+                    
+                    ]),
+                ]
+              ),
+            ),
+
+              
+            
+          ],
+        ),
+      ],
     );
   }
 }
